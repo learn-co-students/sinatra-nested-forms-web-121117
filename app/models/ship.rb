5862c59990bf4a721d8062046723131ebfa56686
josephdlawson21 <josephdlawson21@gmail.com>
2018-01-09 18:47:57 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,27 @@
 class Ship
+  # The ship class should have name, type, and booty attributes, as well as a class method .all that returns all the ships and a class method .clear that deletes all ships.
+
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
+
+
+
 end
