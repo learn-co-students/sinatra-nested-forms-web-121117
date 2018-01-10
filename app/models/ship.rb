f0130002b4f6dcf05cc879f0e5273988154099c4
github username <github email address>
2018-01-10 10:03:57 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(dumb)
+    @name = dumb[:name]
+    @type = dumb[:type]
+    @booty = dumb[:booty]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
 end
